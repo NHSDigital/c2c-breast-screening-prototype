150ca8e6b39bb6ff4d47ac038d349925b9c34771
Mat Johnson <demotive@gmail.com>
2026-07-09 15:02:05 +0100

    A just about working walkthrough

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,18 @@
 const participants = require('./participants')
 
+// currently just for the September Test "clickthru"
 module.exports = {
-  participants
+  participants,
+  stagedCount: 0,
+  clinicName: 'Worthing - test day September 2026',
+  clinicId: 'HWO-NNN-standard-20260501',
+  unitName: 'West Sussex Breast Screening Centre',
+  unitId: 'HWO-static-1',
+  locationName: 'Worthing',
+  locationAddressLine1: "Worthing Hospital",
+  clinicTimespan: "1 September 2026",
+  clinicLength: "1 day",
+  clinicCapacityPercentBooked: "0",
+  clinicCapacityAvailable: "48",
+  clinicCapacityTotal: "48"
 }
