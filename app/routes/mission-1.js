ad38af14b40d81b65e6fb1be0caccb49302f679e
Mat Johnson <demotive@gmail.com>
2026-08-26 15:17:53 +0100

    Moving Mission 1 routes into their own file for simplicity

---
+++ app/routes/mission-1.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const router = express.Router()
+
+// Routes and functions specific to Mission 1 prototypes
+/*
+
+/appviews/september-iteration-2/
+
+*/
+
+router.get('/september-iteration-2/clickthru/04a-example-search-result', function (req, res) {
+  const query = (req.query['search-params'] || '').trim()
+  const allParticipants = (req.session.data.participants && req.session.data.participants.default) || []
+  const normalizedQuery = query.toLowerCase().replace(/\s+/g, ' ')
+  const nhsQuery = normalizedQuery.replace(/\s+/g, '')
+
+  const searchResults = (normalizedQuery ? allParticipants.map((participant, index) => {
+    const nhs = participant.nhs_number.toLowerCase().replace(/\s+/g, '')
+    const name = participant.full_name.toLowerCase()
+    const dob = participant.date_of_birth.toLowerCase()
+
+    if (
+      nhs.includes(nhsQuery) ||
+      name.includes(normalizedQuery) ||
+      dob.includes(normalizedQuery)
+    ) {
+      return Object.assign({}, participant, { participantIndex: index })
+    }
+
+    return null
+  }).filter(Boolean) : allParticipants.map((participant, index) => Object.assign({}, participant, { participantIndex: index })))
+
+  res.render('september-iteration-2/clickthru/04a-example-search-result', {
+    participants: searchResults,
+    searchQuery: query
+  })
+})
+
+router.get('/action/stage/:participantId', function (req, res) {
+  const participants = (req.session.data.participants && req.session.data.participants.default) || []
+  const participantIndex = participants.findIndex((participant) => participant.participantId === req.params.participantId)
+
+  if (participantIndex !== -1) {
+    participants[participantIndex].status = 'staged'
+    req.session.data.stagedCount++
+  }
+
+  res.redirect(req.get('referer') || '/september-iteration-2/clickthru/04-choose-participants')
+});
+
+router.get('/action/unstage/:participantId', function (req, res) {
+  const participants = (req.session.data.participants && req.session.data.participants.default) || []
+  const participantIndex = participants.findIndex((participant) => participant.participantId === req.params.participantId)
+
+  if (participantIndex !== -1) {
+    participants[participantIndex].status = 'unstaged'
+    req.session.data.stagedCount--
+  }
+
+  res.redirect(req.get('referer') || '/september-iteration-2/clickthru/04-choose-participants')
+});
+
+module.exports = router
